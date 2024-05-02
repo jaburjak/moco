c80aaa9ef0a71e95beb02df01d947751e22a9f8d
Jakub Jabůrek <jaburek.jakub@gmail.com>
2024-05-02 14:39:50 +0200

    ci: run MocoD2STestSuite

--- build/test.sh
+++ build/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SUITES=("MocoOpenPonkTestSuite" "MocoO2UTestSuite" "MocoU2DTestSuite")
+SUITES=("MocoOpenPonkTestSuite" "MocoO2UTestSuite" "MocoU2DTestSuite" "MocoD2STestSuite")
 
 for suite in "${SUITES[@]}"; do
 	./pharo/bin/pharo --headless image/openponk-class-editor.image eval "| r | r := $suite new run. (r tests size) = (r passed size)" | grep true
